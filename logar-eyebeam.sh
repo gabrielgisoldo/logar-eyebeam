b66eac452457ba2bc5709da5af0d354847d19f7c
gabriel diniz gisoldo <gabrielgisoldi@gmail.com>
2019-05-14 13:04:35 -0300

    Create logar-eyebeam.sh

---
+++ logar-eyebeam.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+wmctrl_pkg=$(ls /var/log/packages | grep wmctrl)
+
+if [[ ${#wmctrl_pkg} -eq 0 ]]; then
+    echo "Nao tem wmctrl instalado"
+    exit
+fi
+
+wine "C:\Program Files (x86)\CounterPath\eyeBeam 1.5\eyeBeam.exe"  -dial="*1"
+
+sleep 2
+
+WID=""
+while [ "$WID" == "" ]; do
+    # Busca o ID da janela do programa
+    WID=$( wmctrl -lp | grep -P "eyeBeam\b(?=(\n|))" | awk '{print $1}' | xargs printf '%d' )
+    echo "$WID"
+done
+
+# wine "C:\Program Files (x86)\CounterPath\eyeBeam 1.5\eyeBeam.exe"  -dial="*1"
+
+sleep 2
+
+#SUBSTITUA RAMAL PELO NÚMERO DO SEU RAMAL
+xdotool windowactivate --sync "$WID" type --delay 700 RAMAL
+
+sleep 1
+
+#SUBSTITUA SENHA PELA SENHA DO SEU RAMAL.
+#LEMBRE-SE DE MANTER O CERQUILHA (#) NO FIM DA SENHA.
+xdotool windowactivate --sync "$WID" type --delay 700 SENHA#
