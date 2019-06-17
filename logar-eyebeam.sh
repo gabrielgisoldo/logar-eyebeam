5ebc9f6e7ee3dfe0a87c4cf4b648565af4d88090
Gabriel Gisoldo <gabrielgisoldi@gmail.com>
2019-06-17 13:45:31 -0300

    Alteracao no script para ajustar tempo de execucao e para logar automatimente na total ip.

--- logar-eyebeam.sh
+++ logar-eyebeam.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 wmctrl_pkg=$(ls /var/log/packages | grep wmctrl)
 xdotool_pkg=$(ls /var/log/packages | grep xdotool)
+
+#SUBSTITUA ***** PELO NÚMERO DO SEU RAMAL
+RAMAL=*****
+
+#SUBSTITUA ********************** PELA SENHA DO SEU RAMAL.
+SENHA=**********************
+
 if [[ ${#wmctrl_pkg} -eq 0 ]]; then
     echo "Nao tem wmctrl instalado"
     exit
@@ -12,22 +19,48 @@ if [[ ${#xdotool_pkg} -eq 0 ]]; then
 fi
 
 EYEBEAM=$( find ${HOME}/.wine/drive_c/ -type f -iname eyebeam.exe )
-wine "${EYEBEAM}"  -dial="*1" &>/dev/null &
+wine "${EYEBEAM}" -dial="*1" &>/dev/null &
 
 sleep 2
 
 WID=""
-while [ "$WID" == "" ]; do
+TENTATIVAS=0
+while [[ "$WID" == "" || "$WID" == "0" ]]; do
     # Busca o ID da janela do programa
     WID=$( wmctrl -lp | grep -P "eyeBeam\b(?=(\n|))" | awk '{print $1}' | xargs printf '%d' )
-    echo "$WID"
+    if [[ "$WID" != "" || "$WID" != "0" ]]; then
+        echo "$WID"
+    fi
+    ((TENTATIVAS++)) && ((TENTATIVAS==200)) && exit 1
 done
 
 sleep 1
 
-#SUBSTITUA RAMAL PELO NÚMERO DO SEU RAMAL
-xdotool windowactivate --sync "$WID" type --delay 0.1 RAMAL
+xdotool windowactivate --sync "$WID" type --delay 0.1 $RAMAL
 
-#SUBSTITUA SENHA PELA SENHA DO SEU RAMAL.
-#LEMBRE-SE DE MANTER O CERQUILHA (#) NO FIM DA SENHA.
-xdotool windowactivate --sync "$WID" type --delay 0.1 SENHA#
+xdotool windowactivate --sync "$WID" type --delay 0.1 $SENHA#
+
+sleep 5
+
+firefox -new-tab -url http://totalip5.nube/
+
+WID=""
+TENTATIVAS=0
+while [[ "$WID" == "" || "$WID" == "0" ]]; do
+    # Busca o ID da janela do programa
+    WID=$( wmctrl -lp | grep "softphone" | head -n1 | awk '{print $1}' | xargs printf '%d' )
+    if [[ "$WID" != "" || "$WID" != "0" ]]; then
+        echo "$WID"
+    fi
+
+    ((TENTATIVAS++)) && ((TENTATIVAS==200)) && exit 2
+done
+
+sleep 1
+
+xdotool windowactivate --sync "$WID" key CTRL+a
+xdotool windowactivate --sync "$WID" type --delay 10 $RAMAL
+xdotool windowactivate --sync "$WID" key Tab
+xdotool windowactivate --sync "$WID" key CTRL+a
+xdotool windowactivate --sync "$WID" type --delay 10 $SENHA
+xdotool windowactivate --sync "$WID" key Return
